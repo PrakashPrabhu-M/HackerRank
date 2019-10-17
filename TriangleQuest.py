61bf7a7aff11ed6259e333baf4a95af46d581070
PrakashPrabhu-M <54565583+PrakashPrabhu-M@users.noreply.github.com>
2019-10-17 09:46:37 +0530

    Update TriangleQuest.py

--- TriangleQuest.py
+++ TriangleQuest.py
@@ -38,4 +38,8 @@ for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do no
 '''
 for i in range(1,int(input())):
     print([0, 1, 22, 333, 4444, 55555, 666666, 7777777, 88888888, 999999999][i])
+    
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(sum(map(lambda x: i * 10**x, range(i))))
+
 '''
